2a2535b5966a4f317a18cbe57ca86dfa1cd9376c
utegenf <38212992+utegenf@users.noreply.github.com>
2021-04-06 18:47:30 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,25 @@
+library(shinyWidgets)
+library(shiny)
+library(shinyWidgets)
+library(shiny)
+library(plotly)
+library(shinythemes)
+library(DT)
+library(rsconnect)
+# ------------------
+# Main title section
+# ------------------
+
+ui <- navbarPage(
+  "SuicideRate",
+  theme = shinytheme("flatly"),
+  tabPanel(
+    "Main",
+    # App title ----
+    titlePanel(div(
+      windowTitle = "SuicideRateSG",
+      img(src = "sg0.jpg", width = "100%", class = "bg"),
+    )),
+    
+    tags$br(),
 
