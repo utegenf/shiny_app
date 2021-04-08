bbab8724f4e14dc93e8938127e58919f615bad94
utegenf <38212992+utegenf@users.noreply.github.com>
2021-04-08 13:09:36 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,21 +4,19 @@ library(shinyWidgets)
 library(shiny)
 library(plotly)
 library(shinythemes)
-library(DT)
-library(rsconnect)
 # ------------------
 # Main title section
 # ------------------
 
 ui <- navbarPage(
-  "SuicideRate",
+  "Anime Suggestions",
   theme = shinytheme("flatly"),
   tabPanel(
     "Main",
     # App title ----
     titlePanel(div(
-      windowTitle = "SuicideRateSG",
-      img(src = "suicide.jpeg", width = "100%", class = "bg"),
+      windowTitle = "AnimeSG",
+      img(src = "wallpaper.jpeg", width = "100%", class = "bg"),
     )),
     
     tags$br(),
@@ -42,18 +40,153 @@ ui <- navbarPage(
             selectInput(
               "checkYear",
               "Select Year",
-              choices = list("2019", "2018", "2017"),
-              selected = "2018"
+              choices = list("1997", "1998", "2000", "2001", "2002", "2004", "2008", "2010", "2018", "2020", "2021"),
+              selected = "2021"
             )
           ),
           
           mainPanel(
             tabsetPanel(
               type = "tabs",
-              tabPanel("Ranking", tableOutput("datahead")),
+              tabPanel("Score", tableOutput("datahead")),
             ),
             tags$br(),
             tags$br(),
           )
         ),
         tags$hr(),
+        
+        
+        sidebarLayout(
+          sidebarPanel(
+            # ------------------
+            # Data overview filters
+            # ------------------
+            
+            h3("Data Overview"),
+            tags$br(),
+            setSliderColor(c("#2c3e50 ", "#2c3e50"), c(1, 2)),
+            sliderInput(
+              "durationRange",
+              label = "Duration per episode/min",
+              min = 10,
+              max = 50,
+              value = c(10, 50)
+            ),
+            setSliderColor(c("e67e22 ", "#e67e22"), c(1, 2)),
+            sliderInput(
+              "episodeRange",
+              label = "Number of episodes",
+              min = 3,
+              max = 1800,
+              value = c(3, 1800)
+            ),
+            selectInput(
+              "checkYearGroup",
+              "Select Year",
+              choices = data$year,
+              selected = "2018",
+              multiple = TRUE
+            ),
+            
+            checkboxGroupInput("checkYear", label = "Select Year",
+                              choices = list("1997", "1998", "2000", "2001", "2002", "2004", "2008", "2010", "2018", "2020", "2021"),
+                             selected = list("1997", "1998", "2000", "2001", "2002", "2004", "2008", "2010", "2018", "2020", "2021"), inline = TRUE),
+            
+            actionButton("actionDT", "Filter", class = "btn btn-warning"),
+          ),
+          mainPanel(
+            h3("Browse All"),
+            tags$br(),
+            dataTableOutput("myTable"),
+            tags$br(),
+            tags$br(),
+          )
+        ),
+        tags$hr(),
+      ),
+   
+        
+      ################################################
+      #### Panel: Main>Details                    ####
+      ################################################
+      
+      tabPanel(
+        "Details By Producers",
+        h3("Highest scored animes by genre/producer", align = "center"),
+        br(),
+        div(style = "display:vertical-align:center;center-align",
+            fluidRow(
+              column(
+                4,
+                selectInput(
+                  "detailproducers",
+                  label = "Select STUDIO production",
+                  choices = unique(data$Producers),
+                  selected = "DAX Production",
+                  width = 400
+                ),
+              ),
+              column(
+                4,
+                selectInput(
+                  "detailgenre",
+                  "Select Genre",
+                  choices = "",
+                  selected = "",
+                  width = 400
+                )
+              ),
+              column(4,
+                     column(
+                       8,
+                       selectInput(
+                         "detailRating",
+                         "Select Rating",
+                         choices = "",
+                         selected = "",
+                         width = 400
+                       )
+                     ),
+                     column(
+                       4,
+                       tags$br(),
+                       actionButton("detailFilter", "Filter", class = "btn btn-warning btn-sm")
+                     ))
+            )),
+        
+        tags$br(),
+        tags$br(),
+        tags$hr(),
+        tags$br(),
+        
+        fluidRow(
+          column(4, tableOutput("detailTable")),
+          column(4, h5("Average Score", align="center"), plotOutput(outputId = "detailPlot", height = "300px")),
+        ),
+        
+        tags$br(),
+        tags$br(),
+        tags$br(),
+        tags$br(),
+        tags$hr(),
+        tags$br()
+      )
+    )
+  ),
+  
+  
+  
+  ################################################
+  #### Panel: Documentation                   ####
+  ################################################
+  
+  tabPanel("Documentation",
+           fluidPage(htmlOutput("doc"))),
+  
+  ################################################
+  #### Panel: About                           ####
+  ################################################
+  tabPanel("About",
+           fluidPage(htmlOutput("abo")))
+)
